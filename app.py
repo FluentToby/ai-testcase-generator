8e378a148182a9c46ba15c02ffe743e90c05081c
tobybroadbent <tobybroadbent@fluentmoney.co.uk>
2025-07-04 10:22:30 +0100

    Web app structure

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 from generator import generate_test_cases
 import hashlib
 import pandas as pd
+
+st.set_page_config(page_title ="AI Test Case Generator", layout="centered")
+st.title("AI Test Case Generator")
+
